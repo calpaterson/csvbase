43e387f8f5dc89c5c10c5c58f181ebe023e930df
Cal Paterson <cal@calpaterson.com>
2022-04-04 21:32:17 +0100

    Lint fixes

--- tests/test_cors.py
+++ tests/test_cors.py
@@ -5,22 +5,36 @@ from flask import url_for
 import pytest
 
 
-@pytest.mark.parametrize("method, endpoint, endpoint_args", [
-    ("GET", "csvbase.get_table", {"username": "test", "table_name": "test"}),
-    ("POST", "csvbase.create_row", {"username": "test", "table_name": "test"}),
-    ("PUT", "csvbase.update_row", {"username": "test", "table_name": "test", "row_id": 1}),
-    ("GET", "csvbase.get_row", {"username": "test", "table_name": "test", "row_id": 1}),
-    ("DELETE", "csvbase.delete_row", {"username": "test", "table_name": "test", "row_id": 1}),
-    ("PUT", "csvbase.upsert_table", {"username": "test", "table_name": "test"}),
-
-])
+@pytest.mark.parametrize(
+    "method, endpoint, endpoint_args",
+    [
+        ("GET", "csvbase.get_table", {"username": "test", "table_name": "test"}),
+        ("POST", "csvbase.create_row", {"username": "test", "table_name": "test"}),
+        (
+            "PUT",
+            "csvbase.update_row",
+            {"username": "test", "table_name": "test", "row_id": 1},
+        ),
+        (
+            "GET",
+            "csvbase.get_row",
+            {"username": "test", "table_name": "test", "row_id": 1},
+        ),
+        (
+            "DELETE",
+            "csvbase.delete_row",
+            {"username": "test", "table_name": "test", "row_id": 1},
+        ),
+        ("PUT", "csvbase.upsert_table", {"username": "test", "table_name": "test"}),
+    ],
+)
 def test_cors__allowed_urls(client, method, endpoint, endpoint_args):
-    import logging
-    logging.getLogger('flask_cors').level = logging.DEBUG
     url = url_for(endpoint, **endpoint_args)
     resp = client.options(url, headers={"Access-Control-Request-Method": method})
     assert resp.status_code == 200
-    access_control_headers = {k: v for k,v in resp.headers.items() if k.startswith("Access-Control-")}
+    access_control_headers = {
+        k: v for k, v in resp.headers.items() if k.startswith("Access-Control-")
+    }
 
     assert access_control_headers == {
         "Access-Control-Allow-Origin": "*",
@@ -28,5 +42,3 @@ def test_cors__allowed_urls(client, method, endpoint, endpoint_args):
         "Access-Control-Allow-Methods": ANY,
     }
     assert method in access_control_headers["Access-Control-Allow-Methods"]
-    # print(resp.headers)
-    # assert False
