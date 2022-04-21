eb315947324ccac51e786aae132ed70fd0561c9e
Cal Paterson <cal@calpaterson.com>
2022-04-21 21:31:50 +0100

    Proofread and tweak models prior to putting alembic in

--- csvbase/models.py
+++ csvbase/models.py
@@ -10,10 +10,21 @@ from sqlalchemy import (
     func,
 )
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import CheckConstraint
+from sqlalchemy.schema import CheckConstraint, MetaData
 from sqlalchemy.orm import RelationshipProperty, relationship
 
-Base = declarative_base()
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
+metadata = MetaData(naming_convention=naming_convention)
+
+Base = declarative_base(metadata=metadata)
 
 # https://github.com/dropbox/sqlalchemy-stubs/issues/94
 if TYPE_CHECKING:
@@ -28,14 +39,13 @@ METADATA_SCHEMA_TABLE_ARG = {"schema": "metadata"}
 class User(Base):
     __tablename__ = "users"
     __table_args__ = (
-        CheckConstraint("username ~ '^[A-z][-A-z0-9]+$'"),
+        CheckConstraint("username ~ '^[A-z][-A-z0-9]+$'", "username_format"),
+        CheckConstraint("char_length(username) <= 200", "username_length"),
         METADATA_SCHEMA_TABLE_ARG,
     )
 
     user_uuid = Column(PGUUID, primary_key=True)
-    username = Column(
-        satypes.String(length=200), nullable=False, unique=True, index=True
-    )
+    username = Column(satypes.String, nullable=False, unique=True, index=True)
     password_hash = Column(satypes.String, nullable=False)
     timezone = Column(satypes.String, nullable=False)
     registered = Column(satypes.DateTime(timezone=True), nullable=False)
@@ -68,7 +78,9 @@ class APIKey(Base):
 class Table(Base):
     __tablename__ = "tables"
     __table_args__ = (
-        CheckConstraint("table_name ~ '^[A-z][-A-z0-9]+$'"),
+        CheckConstraint("table_name ~ '^[A-z][-A-z0-9]+$'", "table_name_format"),
+        CheckConstraint("char_length(table_name) <= 200", "table_name_length"),
+        CheckConstraint("char_length(caption) <= 200", "caption_length"),
         UniqueConstraint("user_uuid", "table_name"),
         METADATA_SCHEMA_TABLE_ARG,
     )
@@ -84,9 +96,9 @@ class Table(Base):
         ForeignKey("metadata.data_licences.licence_id"),
         nullable=False,
     )
-    table_name = Column(satypes.String(length=200), nullable=False, index=True)
+    table_name = Column(satypes.String, nullable=False, index=True)
     caption = Column(
-        satypes.String(length=200),
+        satypes.String,
         nullable=False,
     )
 
