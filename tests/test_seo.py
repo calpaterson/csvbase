e4d2caf37ed7b9133ca0b619ef4bb77ab55b0043
Cal Paterson <cal@calpaterson.com>
2022-04-24 21:13:46 +0100

    Tighten up test for seo endpoints' cache control

--- tests/test_seo.py
+++ tests/test_seo.py
@@ -2,8 +2,10 @@ def test_robots(client):
     resp = client.get("/robots.txt")
     assert resp.status_code == 200
     assert resp.data == b"Sitemap: http://localhost/sitemap.xml"
+    assert resp.headers["Cache-Control"] == "public, max-age=604800"
 
 
 def test_sitemap(client):
     resp = client.get("/sitemap.xml")
     assert resp.status_code == 200
+    assert resp.headers["Cache-Control"] == "public, max-age=604800"
