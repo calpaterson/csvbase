7b6f24292f093a50b6c6fd784a5efceae6aecd83
Cal Paterson <cal@calpaterson.com>
2022-05-03 21:30:00 +0100

    Remove more heroku hard coding

--- csvbase/db.py
+++ csvbase/db.py
@@ -6,13 +6,7 @@ from sqlalchemy.sql.expression import text
 from .value_objs import DataLicence
 from .models import Base
 
-if "HEROKU" in environ:
-    # we're running on heroku
-    engine = create_engine(
-        environ["DATABASE_URL"].replace("postgres://", "postgresql://")
-    )
-else:
-    engine = create_engine(environ.get("CSVBASE_DB_URL", "postgresql:///csvbase"))
+engine = create_engine(environ.get("CSVBASE_DB_URL", "postgresql:///csvbase"))
 
 
 def make_tables():
