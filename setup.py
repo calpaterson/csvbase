958a93fd6daef844a6dcb81e3fa017c7740a74f0
Cal Paterson <cal@calpaterson.com>
2022-08-30 23:35:03 +0100

    Downgrade pandas to keep Python 3.7 support

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
             "black==22.3.0",
             "bpython~=0.22.1",
             "mypy==0.910",
-            "pandas==1.4.3",
+            "pandas==1.3.5",
             "pytest-flask==1.2.0",
             "pytest==7.1.1",
             "sqlalchemy-stubs==0.4",
