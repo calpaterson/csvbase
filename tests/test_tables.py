c285c30b2b9933d6fc2476b4a92b1b43ca854bbd
Cal Paterson <cal@calpaterson.com>
2022-09-03 07:56:50 +0100

    Clean up tests a little

--- tests/test_tables.py
+++ tests/test_tables.py
@@ -9,19 +9,14 @@ import pytest
 from .utils import test_data_path, random_string
 
 
-def test_csvs_in_and_out(test_user, sesh, client, ten_rows):
-    """Test that a csv can be pulled out, edited, and then posted back"""
-    url = f"/{test_user.username}/{ten_rows}"
-
+def get_df(client, url) -> pd.DataFrame:
     get_resp = client.get(url)
-    df = pd.read_csv(
+    return pd.read_csv(
         StringIO(get_resp.data.decode("utf-8")), index_col="csvbase_row_id"
     )
 
-    df.drop(labels=2, axis="index", inplace=True)  # removed data
-    df.loc[11] = "XI"  # added data
-    df.loc[5] = "FIVE"  # changed data
 
+def put_df(client, user, url, df: pd.DataFrame) -> None:
     buf = StringIO()
     df.to_csv(buf)
 
@@ -30,15 +25,27 @@ def test_csvs_in_and_out(test_user, sesh, client, ten_rows):
         data=buf.getvalue(),
         headers={
             "Content-Type": "text/csv",
-            "Authorization": test_user.basic_auth(),
+            "Authorization": user.basic_auth(),
         },
     )
     assert post_resp.status_code == 200
 
-    second_get_resp = client.get(url)
-    second_df = pd.read_csv(
-        StringIO(second_get_resp.data.decode("utf-8")), index_col="csvbase_row_id"
-    )
+
+
+def test_csvs_in_and_out(test_user, sesh, client, ten_rows):
+    """Test that a csv can be pulled out, edited, and then posted back"""
+    url = f"/{test_user.username}/{ten_rows}"
+
+    df = get_df(client, url)
+    df.drop(labels=2, axis="index", inplace=True)  # removed data
+    df.loc[11] = "XI"  # added data
+    df.loc[5] = "FIVE"  # changed data
+
+    buf = StringIO()
+    df.to_csv(buf)
+
+    put_df(client, test_user, url, df)
+    second_df = get_df(client, url)
 
     assert_frame_equal(df, second_df)
 
@@ -53,32 +60,14 @@ def test_putting_a_table_doesnt_break_adding_new_rows():
 def test_putting_blanks_makes_them_get_autofilled(test_user, sesh, client, ten_rows):
     url = f"/{test_user.username}/{ten_rows}"
 
-    get_resp = client.get(url)
-    df = pd.read_csv(
-        StringIO(get_resp.data.decode("utf-8"))
-    )
-
+    df = get_df(client, url).reset_index()
     df = pd.concat([df, pd.DataFrame({"roman_numeral": ["XI"]})])
     df = df.set_index("csvbase_row_id")
     df.index = df.index.astype(pd.Int64Dtype())
 
-    buf = StringIO()
-    df.to_csv(buf)
+    put_df(client, test_user, url, df)
 
-    post_resp = client.put(
-        url,
-        data=buf.getvalue(),
-        headers={
-            "Content-Type": "text/csv",
-            "Authorization": test_user.basic_auth(),
-        },
-    )
-    assert post_resp.status_code == 200
-
-    second_get_resp = client.get(url)
-    second_df = pd.read_csv(
-        StringIO(second_get_resp.data.decode("utf-8")), index_col="csvbase_row_id"
-    )
+    second_df = get_df(client, url)
 
     # the next number should be 11
     df = df.rename(index={pd.NA: 11})
