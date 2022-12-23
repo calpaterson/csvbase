f212501afbd6b8c2ec51d7348434fc6b395cd625
Cal Paterson <cal@calpaterson.com>
2022-12-23 21:02:44 +0000

    Fix lint

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -7,9 +7,20 @@ from csvbase.svc import peek_csv
 
 test_data = Path(__file__).resolve().parent / "test-data"
 
-@pytest.mark.parametrize("input_filename, expected_columns", [
-    ("abc123.csv", [Column("a", ColumnType.TEXT), Column("b", ColumnType.INTEGER), Column("c", ColumnType.FLOAT)])
-])
+
+@pytest.mark.parametrize(
+    "input_filename, expected_columns",
+    [
+        (
+            "abc123.csv",
+            [
+                Column("a", ColumnType.TEXT),
+                Column("b", ColumnType.INTEGER),
+                Column("c", ColumnType.FLOAT),
+            ],
+        )
+    ],
+)
 def test_peek_csv(input_filename, expected_columns):
     input_path = test_data / input_filename
     with input_path.open() as input_f:
