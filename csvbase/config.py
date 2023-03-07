0b161f6e2e39ed7781a23090249fdba8982f9d0b
Cal Paterson <cal@calpaterson.com>
2023-03-07 22:04:06 +0000

    Simple tests for config

--- csvbase/config.py
+++ csvbase/config.py
@@ -1,7 +1,6 @@
 from logging import getLogger
 from pathlib import Path
-from typing import Optional, IO
-import functools
+from typing import Optional
 from dataclasses import dataclass
 
 import toml
@@ -18,17 +17,21 @@ class Config:
     sentry_dsn: Optional[str]
 
 
-__config__ : Optional[Config] = None
+__config__: Optional[Config] = None
 
 
 def load_config(config_file: Path) -> Config:
     """Loads the configuration at the given path"""
     logger.info("loading config from %s", config_file)
-    with open(config_file, encoding="utf-8") as config_f:
-        as_dict = toml.load(config_f)
+    if config_file.exists():
+        with open(config_file, encoding="utf-8") as config_f:
+            as_dict = toml.load(config_f)
+    else:
+        logger.warning("config file ('%s') not found, using defaults", config_file)
+        as_dict = {}
     return Config(
-        db_url=as_dict["db_url"],
-        environment=as_dict["environment"],
+        db_url=as_dict.get("db_url", "postgresql:///csvbase"),
+        environment=as_dict.get("environment", "local"),
         blog_ref=as_dict.get("blog_ref"),
         secret_key=as_dict.get("secret_key"),
         sentry_dsn=as_dict.get("sentry_dsn"),
@@ -37,12 +40,12 @@ def load_config(config_file: Path) -> Config:
 
 def default_config_file() -> Path:
     """Returns the location of the default config file"""
-
     return Path.home() / ".csvbase.toml"
 
 
 def get_config() -> Config:
     """Returns the config"""
+    global __config__
     if __config__ is None:
         __config__ = load_config(default_config_file())
 
---
+++ tests/test_config.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import toml
+
+from csvbase.config import load_config, Config
+
+
+def test_config_file_not_exist(tmpdir):
+    config = load_config(Path(tmpdir / "config.toml"))
+    assert config == Config(
+        db_url="postgresql:///csvbase",
+        environment="local",
+        blog_ref=None,
+        secret_key=None,
+        sentry_dsn=None,
+    )
+
+
+def test_config_file_basic(tmpdir):
+    config = {"db_url": "postgresql:///csvboth", "environment": "test"}
+    config_file = Path(tmpdir / "config.toml")
+    with open(config_file, "w") as config_f:
+        toml.dump(config, config_f)
+
+    assert load_config(config_file) == Config(
+        db_url="postgresql:///csvboth",
+        environment="test",
+        blog_ref=None,
+        secret_key=None,
+        sentry_dsn=None,
+    )
