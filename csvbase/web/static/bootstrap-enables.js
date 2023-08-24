a75fbb875b95f81c551ceb2b73ff21426ad01a6b
Cal Paterson <cal@calpaterson.com>
2023-08-24 08:52:23 +0100

    Add a comment making it clear where I've pasted code from.
    In case it changes in the future.

--- csvbase/web/static/bootstrap-enables.js
+++ csvbase/web/static/bootstrap-enables.js
@@ -1,4 +1,5 @@
 // This file is just for enabling various bootstrap features
+// https://getbootstrap.com/docs/5.1/components/tooltips/#example-enable-tooltips-everywhere
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
