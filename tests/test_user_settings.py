f3ddf36a62eb9fb629c9459ee35e57eeac3b07cd
equirke <eoghanquirke@gmail.com>
2023-10-07 18:26:42 +0100

    Add user settings test where email is blanked
    Again this html form will still submit fields when blank

--- tests/test_user_settings.py
+++ tests/test_user_settings.py
@@ -25,6 +25,25 @@ def test_user_settings__updating(sesh, client, test_user):
     assert new_user_obj.email == new_email
 
 
+def test_user_settings__updating_delete_email(sesh, client, test_user):
+    set_current_user(test_user)
+
+    settings_url = f"/{test_user.username}/settings"
+    get_resp = client.get(settings_url)
+    assert get_resp.status_code == 200
+
+    new_timezone = "Asia/Tokyo"
+    new_email = ""
+    post_resp = client.post(
+        settings_url, data={"timezone": new_timezone, "email": new_email}
+    )
+    assert post_resp.status_code == 302, post_resp.data
+
+    new_user_obj = svc.user_by_name(sesh, test_user.username)
+    assert new_user_obj.timezone == new_timezone
+    assert new_user_obj.email == new_email
+
+
 def test_nonsense_timezone(sesh, client, test_user):
     set_current_user(test_user)
 
