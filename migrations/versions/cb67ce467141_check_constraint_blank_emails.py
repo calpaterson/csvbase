9e5f504c396ccd3a4003bbbde2afaa7e26aa45c2
equirke <eoghanquirke@gmail.com>
2023-10-08 14:50:03 +0100

    Add check constraint preventing blank emails being submitted

---
+++ migrations/versions/cb67ce467141_check_constraint_blank_emails.py
@@ -0,0 +1,27 @@
+"""check constraint blank emails
+
+Revision ID: cb67ce467141
+Revises: 63cd716e7107
+Create Date: 2023-10-08 14:33:09.338971+01:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "cb67ce467141"
+down_revision = "63cd716e7107"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("DELETE FROM metadata.user_emails WHERE email_address = ''")
+    op.create_check_constraint(
+        "email_address_blank", "user_emails", "email_address <> ''", "metadata"
+    )
+
+
+def downgrade():
+    op.drop_constraint("email_address_blank", "user_emails", "check", "metadata")
