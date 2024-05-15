8bccb85eba420048847af9551186db06ba9b99e6
Cal Paterson <cal@calpaterson.com>
2024-05-15 15:55:34 +0300

    Check in comment around last_checked

--- csvbase/models.py
+++ csvbase/models.py
@@ -179,7 +179,8 @@ class GithubUpstream(Base):
     https_repo_url = Column(satypes.String, nullable=False)
     branch = Column(satypes.String, nullable=False)
     path = Column(satypes.String, nullable=False)
-    last_checked = Column(satypes.DateTime(timezone=True), nullable=False)
+    # FIXME: to come
+    # last_checked = Column(satypes.DateTime(timezone=True), nullable=False)
 
 
 class TableReadme(Base):
