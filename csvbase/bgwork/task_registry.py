79b40386e74961fe15ff0cd7868a50be9886c191
Cal Paterson <cal@calpaterson.com>
2024-07-01 11:33:38 +0300

    Update stripe subscriptions via celery beat

--- csvbase/bgwork/task_registry.py
+++ csvbase/bgwork/task_registry.py
@@ -6,6 +6,7 @@ from logging import getLogger
 
 from celery import Celery
 
+from csvbase.web.billing import svc as billing_svc
 from csvbase.value_objs import GitUpstream
 from csvbase.userdata import PGUserdataAdapter
 from csvbase.sesh import get_sesh
@@ -35,7 +36,7 @@ def update_external_tables() -> None:
 
 
 @celery.task
-def update_external_table(table_uuid: UUID):
+def update_external_table(table_uuid: UUID) -> None:
     sesh = get_sesh()
     git_source = GitSource()
     backend = PGUserdataAdapter(sesh)
@@ -49,10 +50,19 @@ def update_external_table(table_uuid: UUID):
             svc.mark_table_changed(sesh, table.table_uuid)
             sesh.commit()
 
+@celery.task
+def update_stripe_subscriptions() -> None:
+    sesh = get_sesh()
+    billing_svc.initialise_stripe()
+    billing_svc.update_stripe_subscriptions(sesh, full=False)
+
 
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender: Celery, **kwargs) -> None:
     """Sets up the various periodic tasks for celery beat."""
     sender.add_periodic_task(
         timedelta(minutes=30).total_seconds(), update_external_tables.s()
+    )
+    sender.add_periodic_task(
+        timedelta(days=1).total_seconds(), update_stripe_subscriptions.s()
     )
