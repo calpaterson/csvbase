94bc9417b2908693ca2028457d25714511c4c66c
Cal Paterson <cal@calpaterson.com>
2024-09-09 10:58:31 +0300

    UI improvements to replies

--- csvbase/web/static/comments.js
+++ csvbase/web/static/comments.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const commentTextareas = document.querySelectorAll('.comment-textarea');
+    const commentTextarea = document.querySelector('#comment-textarea');
 
-    commentTextareas.forEach(textarea => {
+    if (commentTextarea.value.trim() != ""){
         // Make the textarea fit the text inside.  CSS will be able to do this
         // soon with content-sizing: fixed
-        textarea.style.height = ""; // Reset any existing height styles
-        textarea.style.height = (textarea.scrollHeight + 5) + "px";
-    });
-
-
+        commentTextarea.style.height = ""; // Reset any existing height styles
+        commentTextarea.style.height = (commentTextarea.scrollHeight + 5) + "px";
+        commentTextarea.setSelectionRange(commentTextarea.value.length, commentTextarea.value.length);
+    }
 });
