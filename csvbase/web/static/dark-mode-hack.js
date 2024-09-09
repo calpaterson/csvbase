181dbeba263c226f125502dd38f148c1adf9edd3
Cal Paterson <cal@calpaterson.com>
2024-09-09 09:15:16 +0300

    Dark mode hack needs to be synchronous to avoid FOUC

--- csvbase/web/static/dark-mode-hack.js
+++ csvbase/web/static/dark-mode-hack.js
@@ -1,4 +1,4 @@
-// FIXME: really should not need javascript for this
+// Bootstrap does not currently have a way to do this automatically so custom JS required
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
     document.querySelector('html').setAttribute('data-bs-theme', 'dark');
     document.getElementById('codehilite-stylesheet').href = "/static/codehilite-dark.css";
