2089f07b9a592979ead2b24296c5b3e1853c4c54
Cal Paterson <cal@calpaterson.com>
2024-09-20 10:36:00 +0300

    Fix two tests broken by removing `public` from cache-control

--- tests/test_seo.py
+++ tests/test_seo.py
@@ -11,13 +11,13 @@ def test_robots(client):
     resp = client.get("/robots.txt")
     assert resp.status_code == 200
     assert resp.data == b"Sitemap: http://localhost/sitemap.xml"
-    assert resp.headers["Cache-Control"] == "public, max-age=86400"
+    assert resp.headers["Cache-Control"] == "max-age=86400"
 
 
 def test_sitemap(client):
     resp = client.get("/sitemap.xml")
     assert resp.status_code == 200
-    assert resp.headers["Cache-Control"] == "public, max-age=86400"
+    assert resp.headers["Cache-Control"] == "max-age=86400"
 
     # Check it's valid XML
     with open(test_data_path + "/sitemap.xsd", "rb") as sitemap_xsd_f:
